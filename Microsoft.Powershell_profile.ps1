46b5695e631517e3e16f9da39fa3c96bdec449c7
Jorge Cisneros <cisneros.jorge.a@gmail.com>
2023-08-24 23:02:22 -0500

    cleaned up a few things...

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -9,14 +9,10 @@ Import-Module $home\Documents\WindowsPowerShell\outlook
 # Start my threads
 Start-Job -FilePath $home\Documents\WindowsPowerShell\threads.ps1
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
+# Chocolatey autocomplete
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
-
-function upgradechoc {choco upgrade all -y}
+# Update Chocolatey
+function update {choco upgrade all -y}

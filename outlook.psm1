bde7f0f3c85a6e19889f5ef155c40709e14d89c1
Jorge Cisneros <cisneros.jorge.a@gmail.com>
2023-08-25 18:09:21 -0500

    added outlook.quit()

--- outlook.psm1
+++ outlook.psm1
@@ -9,6 +9,7 @@ function Send-Email {
     $mail.Subject = $subject
     $mail.Body = $body
     $mail.Send()
+    $outlook.Quit()
 }
 
 function Get-Meetings {
@@ -20,7 +21,7 @@ function Get-Meetings {
     $properties = @(
         'Subject',
         'Start',
-        'Duartion',
+        'Duration',
         'Location',
         'Body'
     )
@@ -31,4 +32,5 @@ function Get-Meetings {
     $calendar = $folder.Folders('Calendar')
     $meetings = $calendar.Items | Select-Object -Property $properties 
     $meetings | Where-Object {$_.Start.Month -eq $month -and $_.Start.Day -eq $day -and $_.Start.Year -eq $year}
+    $outlook.Quit()
 }

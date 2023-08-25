dd1996c68cc8e350e8c5d0c117ecb1ba614c2381
Jorge Cisneros <jcisneros@kilgore.edu>
2023-08-25 15:27:22 -0500

    added Get-Meetings to Outlook

--- outlook.psm1
+++ outlook.psm1
@@ -10,3 +10,17 @@ function Send-Email {
     $mail.Body = $body
     $mail.Send()
 }
+
+function Get-Meetings {
+    $today = Get-Date
+    $month = $today.Month
+    $day = $today.Day
+    $year = $today.Year
+    
+    $outlook = New-Object -ComObject Outlook.Application
+    $namespace = $outlook.GetNamespace('MAPI')
+    $folder = $namespace.Folders('cisneros.jorge@outlook.com')
+    $calendar = $folder.Folders('Calendar')
+    $meetings = $calendar.Items | Select-Object -Property Subject, Start, Duration, Location, Body
+    $meetings | Where-Object {$_.Start.Month -eq $month -and $_.Start.Day -eq $day -and $_.Start.Year -eq $year}
+}

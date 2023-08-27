4ff483d676dbebdc81a5451ca7d2a82494373f74
server <cisneros.jorge.a@gmail.com>
2023-08-26 23:24:29 -0500

    fixed module import

--- Weather/weather.py
+++ Weather/weather.py
@@ -1,4 +1,11 @@
-import requests
+import subprocess
+import sys
+try:
+    import requests
+except:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
+finally:
+    import requests
 import json
 
 # daily response object

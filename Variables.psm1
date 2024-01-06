e4fb09205ccc03d1668cb3c1dd0b4faa85ebdfea
Jorge Cisneros <cisneros.jorge.a@gmail.com>
2024-01-05 19:10:40 -0600

    added TEXT to leggo

--- Variables.psm1
+++ Variables.psm1
@@ -16,6 +16,7 @@ $global:CHROME = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 $global:OUTLOOK = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
 $global:ONENOTE = "C:\Program Files\Microsoft Office\root\Office16\ONENOTE.EXE"
 $global:CRD = "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Chrome Apps\Chrome Remote Desktop.lnk"
+$global:TEXT = "C:\Users\cisne\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Chrome Apps\Messages.lnk"
 
 # Websites
 $global:YOUTUBE = "https://www.youtube.com/"

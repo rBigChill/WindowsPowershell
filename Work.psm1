234ee3146fbaba8b3ad8f520af1956bafac27a7a
Jorge Cisneros <jcisneros@kilgore.edu>
2024-05-08 13:41:30 -0500

    fixed Work module

--- Work.psm1
+++ Work.psm1
@@ -1,20 +1,22 @@
-Add-Type -AssemblyName PresentationFramework
-
-Function ItIs($hour, $min){
-	$now = Get-Date
-	if ($now.Hour -eq $hour -and $now.Minute -eq $min) {
-		return $true
-	}
-	return $false
-}
-
 Function here {
     Start-Job -ScriptBlock {
+
+        Add-Type -AssemblyName PresentationFramework
+
+        Function ItIs($hour, $min) {
+            $now = Get-Date
+            if ($now.Hour -eq $hour -and $now.Minute -eq $min) {
+                return $true
+            }
+            return $false
+        }
+
         while ($true) {
             Switch ($true) {
                 (ItIs 11 55) {[System.Windows.MessageBox]::Show("LUNCH"); Start-Sleep -Seconds 60}
                 (ItIs 16 55) {[System.Windows.MessageBox]::Show("GO HOME"); Start-Sleep -Seconds 60}
             }
+            Start-Sleep -Seconds 1
         }
     }
 }

1617e528bf7eee03fbf8b5ef0ff02a81276ace9c
server <cisneros.jorge.a@gmail.com>
2024-05-29 23:24:33 -0500

    added function cpu'ps | sort -Descending CPU | more'to Functions

--- Functions.psm1
+++ Functions.psm1
@@ -1,6 +1,7 @@
 # Personal Shell Functions
 Function off {shutdown /s /t 0}
 Function restart {Restart-Computer}
+Function cpu {ps | sort -Descending CPU | more}
 Function lock {rundll32.exe user32.dll,LockWorkStation}
 Function bin {Clear-Recyclebin -Force}
 Function su {start-process powershell -verb runas}

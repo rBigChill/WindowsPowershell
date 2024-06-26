f8be7da22a196ca28473ebcb6f263a21ad541e0f
Jorge Cisneros <jcisneros@kilgore.edu>
2024-06-26 08:56:25 -0500

    printing one news sourse per 60 second interval

--- Dash.psm1
+++ Dash.psm1
@@ -7,34 +7,35 @@ Function Dash {
     $h = hacker -d
     $n = newsapi -d
 
+    Function header {
+        Clear
+        $d = Get-Date
+        $date = $d.ToString("dddd, MMMM dd, yyyy HH:mm")
+        Write-Host $date`n; Sleep 2
+        Write-Host "Weather: $w`n"; Sleep 2
+    }
+
     while ($true) {
         $span = New-TimeSpan -Start $d -End (Get-Date)
         switch ($true) {
             (($span).Hours -gt 1) {
-                Clear
-                $d = Get-Date
                 $w = weather -n
                 $r = reddit -d
                 $h = hacker -d
                 $n = newsapi -d
-                $date = $d.ToString("dddd, MMMM dd, yyyy HH:mm")
-                Write-Host $date`n; Sleep 2
-                Write-Host "Weather: $w`n"; Sleep 2
-                ForEach ($a in $r) {Write-Host $a; Sleep 1}
-                ForEach ($a in $h) {Write-Host $a; Sleep 2}
-                ForEach ($a in $n) {Write-Host $a; Sleep 2}
-                Sleep 1
             }
             Default {
-                Clear
-                $d = Get-Date
-                $date = $d.ToString("dddd, MMMM dd, yyyy HH:mm")
-                Write-Host $date`n; Sleep 2
-                Write-Host "Weather: $w`n"; Sleep 2
-                ForEach ($a in $r) {Write-Host $a; Sleep 2}
-                ForEach ($a in $h) {Write-Host $a; Sleep 2}
-                ForEach ($a in $n) {Write-Host $a; Sleep 2}
-                Sleep 1
+                header
+                ForEach ($a in $r) {Write-Host $a}
+                Sleep 60
+
+                header
+                ForEach ($a in $h) {Write-Host $a}
+                Sleep 60
+
+                header
+                ForEach ($a in $n) {Write-Host $a}
+                Sleep 60
             }
         }
     }

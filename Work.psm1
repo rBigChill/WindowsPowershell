cb379fe2e4b5d2dd34f02285767e6fd27da55b50
Jorge Cisneros <jcisneros@kilgore.edu>
2024-08-12 16:24:23 -0500

    added employee and student lookups

--- Work.psm1
+++ Work.psm1
@@ -77,3 +77,157 @@ Function running {
 Function stopped {
     service | where {$_.Status -eq "Stopped"}
 }
+
+Function Student {
+    param([int]$ID)
+
+    if (Get-Module -ListAvailable SqlServer) {
+        Import-Module -Name SqlServer
+    } else {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        Install-PackageProvider -Name NuGet -Force
+        Install-Module -Name SqlServer -Scope CurrentUser -Force
+        Import-Module -Name SqlServer
+    }
+
+    $SqlServer = "KCL-SQL01"
+    $SqlDBName = "TmsEPrd"
+    $SchemaName = "dbo"
+
+    function Student-Master {
+        param(
+            [int]$ID
+        )
+        Invoke-Sqlcmd -ServerInstance $SqlServer -Encrypt Optional -Query "
+            SELECT
+                'ID' = SM.ID_NUM,
+                'Last' = NM.LAST_NAME,
+                'First' = NM.FIRST_NAME,
+                'Middle' = NM.MIDDLE_NAME,
+                'Active' = IS_STUDENT_ACTIVE,
+                'Start Year' = SM.ENTRANCE_YR,
+                'Start Term' = SM.ENTRANCE_TRM
+            FROM $SqlDBName.$SchemaName.STUDENT_MASTER SM
+                JOIN $SqlDBName.$SchemaName.NameMaster NM ON SM.ID_NUM = NM.ID_NUM
+            WHERE SM.ID_NUM = $ID"
+    }
+
+    function Student-Courses {
+        param(
+            [int]$ID
+        )
+        Invoke-Sqlcmd -ServerInstance $SqlServer -Encrypt Optional -Query "
+            SELECT
+                Top 4
+                'ID' = ID_NUM,
+                'Year' = YR_CDE,
+                'Term' = TRM_CDE,
+                'Course' = CRS_TITLE,
+                'Add' = ADD_DTE,
+                'Start' = BEGIN_DTE,
+                'End' = END_DTE
+            FROM $SqlDBName.$SchemaName.STUDENT_CRS_HIST
+            WHERE ID_NUM = $ID
+            ORDER BY YR_CDE DESC"
+    }
+
+    function Lookup {
+        param(
+            [int]$ID
+        )
+        Get-ADUser $ID
+        Student-Master -ID $ID
+        Student-Courses -ID $ID
+    }
+
+    try {
+        Lookup -ID $ID
+    }
+    catch {
+        $errorMessage = "Error occurred: $_"
+        Write-Host $errorMessage
+    }
+}
+
+Function Employee {
+    param(
+        [string]$first,
+        [string]$last
+    )
+
+    if (Get-Module -ListAvailable SqlServer) {
+        Import-Module -Name SqlServer
+    } else {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        Install-PackageProvider -Name NuGet -Force
+        Install-Module -Name SqlServer -Scope CurrentUser -Force
+        Import-Module -Name SqlServer
+    }
+
+    $SqlServer = "KCL-SQL01"
+    $SqlDBName = "TmsEPrd"
+    $SchemaName = "dbo"
+
+    function Name-Master {
+        param(
+            [string]$first,
+            [string]$last
+        )
+        Invoke-Sqlcmd -ServerInstance $SqlServer -Encrypt Optional -Query "
+            SELECT
+	            'ID' = NM.ID_NUM,
+	            'Last' = LAST_NAME,
+	            'First' = FIRST_NAME,
+	            'Middle' = MIDDLE_NAME,
+	            'Active' = ACT_INACT_STS,
+	            'Confirmed' = DTE_CONFIRMED,
+	            'Hired' = ORIG_HIRE_DTE,
+	            'Start' = FULLTIME_START_DTE,
+	            'Group' = GRP_CDE,
+	            'Termed' = TERMINATION_DTE
+	        FROM $SqlDBName.$SchemaName.NameMaster NM
+		        JOIN $SqlDBName.$SchemaName.EMPL_MAST EM ON EM.ID_NUM = NM.ID_NUM
+	        WHERE NM.FIRST_NAME = '$first' AND NM.LAST_NAME = '$last'"
+    }
+
+    function OnGoing-Load {
+        param(
+            [string]$first,
+            [string]$last
+        )
+        Invoke-Sqlcmd -ServerInstance $SqlServer -Encrypt Optional -Query "
+            SELECT
+	            'Operation' = OperationDate,
+	            'Type' = OperationType,
+	            'Last' = LastName,
+	            'First' = FirstName,
+	            'Middle' = MiddleName,
+	            'Email' = WorkEmailID,
+	            'Phone' = WorkPhone,
+	            'Code' = AgencyCode,
+	            'User' = AgencyUserID,
+	            'Add' = AddApplicationAccess,
+	            'Remove' = RemoveApplicationAccess
+            FROM $SqlDBName.$SchemaName.KCT_003580_ONGOING_LOAD
+	        WHERE FirstName = '$first' AND LastName = '$last'"
+    }
+
+    function Lookup {
+        param(
+            [string]$first,
+            [string]$last
+        )
+        $initial = $first[0]
+        Get-ADUser $initial$last
+        Name-Master -first $first -last $last
+        OnGoing-Load -first $first -last $last
+    }
+
+    try {
+        Lookup -first $first -last $last
+    }
+    catch {
+        $errorMessage = "Error occurred: $_"
+        Write-Host $errorMessage
+    }
+}

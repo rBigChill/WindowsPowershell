6feaea42c078ac3abd590ad15cae64233c47b2e7
Jorge Cisneros <jcisneros@kilgore.edu>
2024-08-13 08:47:13 -0500

    added to variable to send-email function

--- outlook.psm1
+++ outlook.psm1
@@ -1,11 +1,12 @@
 function Send-Email {
     param(
+    [string]$to,
     [string]$subject,
     [string]$body
     )
     $outlook = New-Object -ComObject Outlook.Application
     $mail = $outlook.CreateItem(0)
-    $mail.to = "cisneros.jorge.a@gmail.com"
+    $mail.to = $to
     $mail.Subject = $subject
     $mail.Body = $body
     $mail.Send()

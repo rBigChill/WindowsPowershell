af28e954153112558f3378e63d158110d8b5d861
Jorge Cisneros <jcisneros@uttyler.edu>
2025-04-23 13:21:27 -0500

    added functions to change into specific work directories

--- Work.psm1
+++ Work.psm1
@@ -1,4 +1,5 @@
 #Author: Jorge Cisneros
 
 # Purpose: Functions to use at work
-TEST
+Function bi {cd \\uttyler.net\Data\SharedFolders\BusinessIntelligence}
+Function pir {cd \\uttyler.net\Data\SharedFolders\pir}

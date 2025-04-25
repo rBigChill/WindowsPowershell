79ee1f83cc69d6bba18d668961b4d8c2c40cfd8b
Jorge Cisneros <jcisneros@uttyler.edu>
2025-04-25 11:59:10 -0500

    added .ora file location

--- Work.psm1
+++ Work.psm1
@@ -3,3 +3,4 @@
 # Purpose: Functions to use at work
 Function bi {cd \\uttyler.net\Data\SharedFolders\BusinessIntelligence}
 Function pir {cd \\uttyler.net\Data\SharedFolders\pir}
+Function ora {cd 'C:\app\client\jcisneros\product\19.0.0\client_1\Network\Admin'}

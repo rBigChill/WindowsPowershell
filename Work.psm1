8b33356713ef59c54ee657be968b338cbb92cda8
Jorge Cisneros <jcisneros@uttyler.edu>
2025-04-25 14:07:27 -0500

    added knowledge base link

--- Work.psm1
+++ Work.psm1
@@ -1,7 +1,8 @@
 #Author: Jorge Cisneros
 
 # Purpose: Functions to use at work
-Function bi {cd \\uttyler.net\Data\SharedFolders\BusinessIntelligence}
-Function pir {cd \\uttyler.net\Data\SharedFolders\pir}
+Function bi {cd '\\uttyler.net\Data\SharedFolders\BusinessIntelligence'}
+Function pir {cd '\\uttyler.net\Data\SharedFolders\pir'}
 Function ora {cd 'C:\app\client\jcisneros\product\19.0.0\client_1\Network\Admin'}
-Function share {start https://uthsct.sharepoint.com/sites/InformationAnalysis}
+Function share {start 'https://uthsct.sharepoint.com/sites/InformationAnalysis'}
+Function know {start 'https://uthsct.sharepoint.com/sites/InformationAnalysis/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FInformationAnalysis%2FShared%20Documents%2FIA%20Department%20Knowledge%20Base&viewid=8148821b%2D67dd%2D4876%2D8fe1%2Db66791a9653b'}

d116cfb04918860aa14a7ca449bf095f28f48663
Jorge Cisneros <cisneros.jorge.a@gmail.com>
2025-04-29 08:26:08 -0500

    working out the work module

--- Work.psm1
+++
@@ -1,9 +0,0 @@
-#Author: Jorge Cisneros
-
-# Purpose: Functions to use at work
-Function bi {cd '\\uttyler.net\Data\SharedFolders\BusinessIntelligence'}
-Function pir {cd '\\uttyler.net\Data\SharedFolders\pir'}
-Function ora {cd 'C:\app\client\jcisneros\product\19.0.0\client_1\Network\Admin'}
-Function share {start 'https://uthsct.sharepoint.com/sites/InformationAnalysis'}
-Function know {start 'https://uthsct.sharepoint.com/sites/InformationAnalysis/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FInformationAnalysis%2FShared%20Documents%2FIA%20Department%20Knowledge%20Base&viewid=8148821b%2D67dd%2D4876%2D8fe1%2Db66791a9653b'}
-Function track {start 'https://uthsct.sharepoint.com/sites/InformationAnalysis/Lists/IA%20ProjectTask%20Tracker/Outstanding.aspx?newTargetListUrl=%2Fsites%2FInformationAnalysis%2FLists%2FIA%20ProjectTask%20Tracker&viewpath=%2Fsites%2FInformationAnalysis%2FLists%2FIA%20ProjectTask%20Tracker%2FOutstanding%2Easpx&viewid=571081ce%2Ddb21%2D474e%2Db9bf%2D7b2ac2ed7b9f'}
